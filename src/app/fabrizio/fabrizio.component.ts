da359d26539ea61912b9f71cb5ba198b882392d8
walter <waltermanfredi93@gmail.com>
2019-11-26 14:41:52 +0100

    tour heroes

---
+++ src/app/fabrizio/fabrizio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fabrizio',
+  templateUrl: './fabrizio.component.html',
+  styleUrls: ['./fabrizio.component.css']
+})
+export class FabrizioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
