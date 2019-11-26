f73f28f2e082fd69b93c3cfdc89925ee514bf726
walter <waltermanfredi93@gmail.com>
2019-11-26 14:55:17 +0100

    tour heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FabrizioComponent } from './fabrizio/fabrizio.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FabrizioComponent,
     HeroesComponent
   ],
   imports: [
